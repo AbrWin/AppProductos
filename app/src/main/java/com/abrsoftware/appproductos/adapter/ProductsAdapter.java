b5b21b223d577e89a76a1bd2d28aeb0a0b2bd4ca
Abraham Sánchez Juárez <AbrWin@la-MacBook-Pro-de-Abraham.local>
2016-09-28 18:39:26 -0500

    se empieza con adapter de Producto

--- app/src/main/java/com/abrsoftware/appproductos/adapter/ProductsAdapter.java
+++ app/src/main/java/com/abrsoftware/appproductos/adapter/ProductsAdapter.java
@@ -1,13 +1,18 @@
 package com.abrsoftware.appproductos.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.abrsoftware.appproductos.R;
 import com.abrsoftware.appproductos.products.domain.model.Product;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.List;
 
@@ -15,18 +20,47 @@ import java.util.List;
  * Created by AbrWin on 27/09/16.
  */
 
-public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ProductsHolder> {
+public class ProductsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Product> mProducts;
     private ProductItemListener mItemListener;
 
-    @Override
-    public ProductsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    private final static int TYPE_PRODUCT = 1;
+    private final static int TYPE_LOADING_MORE = 2;
+
+    public ProductsAdapter(List<Product> mProducts, ProductItemListener itemListener) {
+        setList(mProducts);
+        mItemListener = itemListener;
     }
 
     @Override
-    public void onBindViewHolder(ProductsHolder holder, int position) {
+    public int getItemViewType(int position) {
+        if(position < getDataItemCount() && getDataItemCount() > 0){
+            return TYPE_PRODUCT;
+        }
+        return TYPE_LOADING_MORE;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view;
+
+
+        if (viewType == TYPE_LOADING_MORE) {
+            view = layoutInflater.inflate(R.layout.item_loading_footer, parent, false);
+            return new LoadingMoreHolder(view);
+        }
+
+        view = layoutInflater.inflate(R.layout.item_product, parent, false);
+        return new ProductsHolder(view, mItemListener);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
     }
 
@@ -34,6 +68,11 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
     public int getItemCount() {
         return 0;
     }
+
+    private void setList(List<Product> notes) {
+        mProducts = checkNotNull(notes);
+    }
+
 
     public class ProductsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
@@ -47,10 +86,10 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
         public ProductsHolder(View itemView, ProductItemListener listener) {
             super(itemView);
             mItemListener = listener;
-            name = (TextView)itemView.findViewById(R.id.product_name);
-            price = (TextView)itemView.findViewById(R.id.product_price);
-            unitsInStock = (TextView)itemView.findViewById(R.id.units_in_stock);
-            featuredImage = (ImageView)itemView.findViewById(R.id.product_featured_image);
+            name = (TextView) itemView.findViewById(R.id.product_name);
+            price = (TextView) itemView.findViewById(R.id.product_price);
+            unitsInStock = (TextView) itemView.findViewById(R.id.units_in_stock);
+            featuredImage = (ImageView) itemView.findViewById(R.id.product_featured_image);
             itemView.setOnClickListener(this);
         }
 
@@ -60,13 +99,27 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
             Product product = getItem(position);
             mItemListener.onProductClick(product);
         }
-        public Product getItem(int position){
+
+        public Product getItem(int position) {
             return mProducts.get(position);
         }
 
     }
 
-    public interface ProductItemListener{
+    private class LoadingMoreHolder extends RecyclerView.ViewHolder {
+        public ProgressBar progress;
+
+        public LoadingMoreHolder(View view) {
+            super(view);
+            progress = (ProgressBar) view.findViewById(R.id.progressBar);
+        }
+    }
+
+    public interface ProductItemListener {
         void onProductClick(Product clickedNote);
+    }
+
+    private int getDataItemCount(){
+        return mProducts.size();
     }
 }
